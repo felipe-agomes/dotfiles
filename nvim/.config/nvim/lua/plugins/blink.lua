dfcf2ec2c8f088b09fcf1fefb4c87a47d953734c
Felipe de Almeida Gomes <falmeidagomes13@gmail.com>
2026-01-27 13:44:42 -0300

    fix(neovim): add <Tab> to accept completion

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -26,7 +26,9 @@ return {
 		-- C-k: Toggle signature help (if signature.enabled = true)
 		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = { preset = "default" },
+		keymap = {
+			["<Tab>"] = { "accept", "fallback" },
+		},
 
 		appearance = {
 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
