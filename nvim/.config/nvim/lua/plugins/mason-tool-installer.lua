4a56edcbe8a12d7dee50900a00d5e47917d3a9cb
Felipe de Almeida Gomes <falmeidagomes13@gmail.com>
2026-03-25 14:30:33 -0300

    core(neovim): add mason auto install

---
+++ nvim/.config/nvim/lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,13 @@
+return {
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	opts = {
+		ensure_installed = {
+			"stylua",
+			"google-java-format",
+			"prettierd",
+			"sql-formatter",
+			"xmlformatter",
+			"beautysh",
+		},
+	},
+}
