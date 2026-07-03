2a2f86f7ef0897552554b66c9456fec32c97b0c3
Felipe de Almeida Gomes <falmeidagomes13@gmail.com>
2026-07-03 11:47:52 -0300

    feat(nvim): add bufferline commands

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -6,6 +6,22 @@ return {
 	config = function(_, opts)
 		require("bufferline").setup(opts)
 
+		vim.keymap.set("n", "<leader>bd", function()
+			vim.api.nvim_buf_delete(0, { force = false })
+		end, { desc = "Delete Buffer" })
+
+		vim.keymap.set("n", "<leader>bo", function()
+			vim.cmd("BufferLineCloseOthers")
+		end, { desc = "Delete Buffers others" })
+
+		vim.keymap.set("n", "<leader>bl", function()
+			vim.cmd("BufferLineCloseLeft")
+		end, { desc = "Delete Buffers to the Left" })
+
+		vim.keymap.set("n", "<leader>br", function()
+			vim.cmd("BufferLineCloseRight")
+		end, { desc = "Delete Buffers to the Right" })
+
 		vim.keymap.set("n", "H", function()
 			vim.cmd("BufferLineCyclePrev")
 		end)
