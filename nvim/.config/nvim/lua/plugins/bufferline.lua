d2f7012ce53f0414e948ccaceebeda62f6310d76
Felipe de Almeida Gomes <falmeidagomes13@gmail.com>
2026-07-22 17:44:14 -0300

    core(nvim): config

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -11,19 +11,19 @@ return {
 			local buf = vim.api.nvim_get_current_buf()
 			vim.cmd("BufferLineCycleNext")
 			vim.api.nvim_buf_delete(buf, { force = true })
-		end, { desc = "Delete Buffer" })
+		end, { desc = "Buffer: Delete current" })
 
 		vim.keymap.set("n", "<leader>bo", function()
 			vim.cmd("BufferLineCloseOthers")
-		end, { desc = "Delete Buffers others" })
+		end, { desc = "Buffer: Delete others" })
 
 		vim.keymap.set("n", "<leader>bl", function()
 			vim.cmd("BufferLineCloseLeft")
-		end, { desc = "Delete Buffers to the Left" })
+		end, { desc = "Buffer: Delete to the Left" })
 
 		vim.keymap.set("n", "<leader>br", function()
 			vim.cmd("BufferLineCloseRight")
-		end, { desc = "Delete Buffers to the Right" })
+		end, { desc = "Buffer: Delete to the Right" })
 
 		vim.keymap.set("n", "H", function()
 			vim.cmd("BufferLineCyclePrev")
@@ -34,11 +34,11 @@ return {
 		end)
 
 		vim.keymap.set("n", "[B", function()
-			vim.cmd("BufferLineMoveNext")
-		end, { desc = "Move buffer prev" })
+			vim.cmd("BufferLineMovePrev")
+		end, { desc = "Buffer: Move prev" })
 
 		vim.keymap.set("n", "]B", function()
-			vim.cmd("BufferLineMovePrev")
-		end, { desc = "Move buffer next" })
+			vim.cmd("BufferLineMoveNext")
+		end, { desc = "Buffer: Move next" })
 	end,
 }
